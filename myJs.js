d963992c80997eff57e1292e415f2676f3fa608a
Alex <yamargin@gmail.com>
2021-12-03 17:56:40 +0300

    types

--- myJs.js
+++ myJs.js
@@ -1,3 +1,26 @@
+// string, number, boolean, undefined, null, object, BigInt, Sympol 
+
+const name = 'Oleg'
+
+const number = 12
+
+let variableOne
+
+let variableTwo = null
+
+console.log('type of the variabl', typeof(variableOne))
+
+let inStock = true
+
+const car = {
+    brand: 'BMW',
+    color: 'black',
+    year: 2022,
+    inStock: true
+}
+
+console.log('brand:', car.brand)
+
 // var let const
 
 let score 
